b257c5b7af78101c76f35ab731106cc574da2f17
Zeth <zeth.leonardo@protonmail.com>
2020-03-16 19:47:50 +0400

    Add BloC patterns.

---
+++ lib/data/model/user.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String email;
+  final String name;
+  final String username;
+  final String joinedDate;
+  final int roleId;
+
+  User({
+    this.email,
+    this.name,
+    this.username,
+    this.joinedDate,
+    this.roleId,
+  });
+
+  @override
+  List<Object> get props => [
+    email,
+    name,
+    username,
+    joinedDate,
+    roleId,
+  ];
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      email: json['email'],
+      name: json['name'],
+      username: json['username'],
+      joinedDate: json['joined_date'],
+      roleId: json['role_id'],
+    );
+  }
+}
