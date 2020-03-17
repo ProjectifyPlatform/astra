7b5d3a9b616e84448724073c5659cbe37a125ad3
Zeth <zeth.leonardo@protonmail.com>
2020-03-17 16:57:55 +0400

    Implemented BLoC delegates.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+import 'package:astra/data/auth_repository.dart';
+import 'package:astra/widgets/loadingIndicator.dart';
 
-class MyApp extends StatelessWidget {
+import 'package:astra/pages/auth/login_page.dart';
+import 'package:astra/pages/home_page.dart';
+import 'package:astra/pages/splash_page.dart';
+
+
+import 'package:astra/bloc/auth/auth_bloc.dart';
+
+class AppBlocDelegate extends BlocDelegate {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TemporaryHome(),
-    );
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print(error);
   }
 }
 
-class TemporaryHome extends StatelessWidget {
-  
+void main() {
+  BlocSupervisor.delegate = AppBlocDelegate();
+  final authRepository = APIAuthRepository();
+
+  runApp(
+    BlocProvider<AuthBloc>(
+      create: (context) {
+        return AuthBloc(repository: authRepository)
+          ..add(AppStarted());
+      },
+      child: App(authRepository: authRepository),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  final AuthRepository authRepository;
+
+  App({Key key, @required this.authRepository}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Hello World!"),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    home: BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        if (state is AuthUninitialized) {
+          return SplashPage();
+        }
+
+        if (state is Authenticated) {
+          return HomePage();
+        }
+
+        if (state is Unauthenticated) {
+          return LoginPage(authRepository: authRepository);
+        }
+
+        // State is Authenticating
+        return LoadingIndicator();
+      },
+    ),
+  );
 }
